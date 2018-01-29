deda78ef046df1efec9a17b6643e36157d2c4c14
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-29 10:22:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Finance from "financejs"
+
+
+const finance = new Finance()
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      amortisation: ""
+    }
+  }
+
+  handleChange(e) {
+    e.preventDefault()
+    this.setState({
+      amortisation: finance.AM(e.target.value, 7.5, 5, 0)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <label>Amortisation:</label>
+        <input type="text" onChange={this.handleChange} />
+        <p>{this.state.amortisation}</p>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
