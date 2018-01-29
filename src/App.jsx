1b674fa98305d7db8ccaa6d20aef28f9d0a34c56
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-29 10:32:54 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Finance from 'financejs';
+// import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
-import Finance from "financejs"
-import styled from "styled-components"
 
+import {Row, Col} from './grid';
 
 const finance = new Finance()
-
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
     this.state = {
-      amortisation: ""
+      amortisation: ''
     }
   }
 
@@ -28,17 +28,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <label>Amortisation:</label>
-        <input type="text" onChange={this.handleChange} />
+        <input type="text" onChange={this.handleChange}/>
         <p>{this.state.amortisation}</p>
-        <div>
-
-        </div>
-      </div>
-    );
+        <div></div>
+     </div> );
   }
 }
 
