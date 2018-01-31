2a9ce51deba0b9aeb37b2b0d0e0918978df054ef
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-31 16:11:24 +0100

    fixed bugs and added redu forms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 import Finance from 'financejs'
 import styled from 'styled-components'
-import { Field, reduxForm } from 'redux-form'
-import compose from "recompose"
+import {Field, reduxForm} from 'redux-form'
+import {compose} from "recompose"
 
 import logo from './logo.svg'
 import './App.css'
@@ -10,8 +10,7 @@ import {Row, Col} from './grid'
 
 const finance = new Finance()
 
-
-const Input = styled.input`
+const Input = styled.input `
 
 `
 
@@ -32,37 +31,35 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-    <form>
-      <Row>
-        <Col>
-          <label>Wage 1:</label>
-            <Field></Field>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <label>Wage 2:</label>
-          <input type="text" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <label>Amortisation:</label>
-          <input type="text" onChange={this.handleChange}/>
-          <p>{this.state.amortisation}</p>
-        </Col>
-      </Row>
-    </form>
-     </div> )
+    const {handleSubmit} = this.props
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <Row>
+          <Col>
+            <label>Wage 1:</label>
+            <Field component={Input} name="wage.1"></Field>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Wage 2:</label>
+            <Field component={Input} name="wage.2"></Field>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Amortisation:</label>
+            <input type="text" name="amortisation" onChange={this.handleChange}/>
+            <p>{this.state.amortisation}</p>
+          </Col>
+        </Row>
+      </form>
+    </div>)
   }
 }
 
-export default compose(App)(
-  reduxForm({ form: "monthlySpendingsForm" })
-)
+export default compose(App)(reduxForm({form: "monthlySpendingsForm"}))
