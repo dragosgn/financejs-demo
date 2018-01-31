68b25a16ee1f176836cda8515534d05fe2efffeb
Dragos Nedelcu <dragosgn@gmail.com>
2018-01-31 15:54:31 +0100

    added reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import {Provider} from "react-redux"
+
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import monthlySpendingsFormReducer from "./reducers/monthlySpendingsForm"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootReducer = combineReducers({
+  form: formReducer.plugin({
+    monthlySpendingsForm: monthlySpendingsFormReducer
+  }),
+})
+
+const store = createStore(rootReducer)
+
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'))
 registerServiceWorker()
